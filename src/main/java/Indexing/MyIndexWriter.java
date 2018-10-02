3f3f38915c440d8bb9438c5945f57dd46d20df17
TonyZ <i@cpttony.me>
2018-10-02 19:05:38 -0400

    Some basic ideas on processing index.

--- src/main/java/Indexing/MyIndexWriter.java
+++ src/main/java/Indexing/MyIndexWriter.java
@@ -6,26 +6,40 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 public class MyIndexWriter {
 
-    private BufferedWriter indexWriter;
+    /**
+     * Prefix path for storing index data
+     */
+    private String indexPath;
+
+    /**
+     * Document No -> My Id indexer
+     */
+    private LinkedList<String> docIdIndex = new LinkedList<>();
+    private long totalNumOfDocument = 0;
+
+    /**
+     * Map each term by its length
+     */
+    private HashMap<Integer, BufferedWriter> termWriterMap = new HashMap<>(50);
 
     /**
      * This constructor should initiate the FileWriter to output your index files
      * remember to close files if you finish writing the index
      */
     public MyIndexWriter(String type) throws IOException {
-        String path;
         if (type.equals("trecweb")) {
-            path = Path.IndexWebDir;
+            this.indexPath = Path.IndexWebDir;
         } else if (type.equals("trectext")) {
-            path = Path.IndexTextDir;
+            this.indexPath = Path.IndexTextDir;
         } else {
             throw new IOException("Type error");
         }
-        if (!new File(path).mkdirs()) throw new IOException("mkdir failed");
-        this.indexWriter = new BufferedWriter(new FileWriter(path + "idx", false));
+        if (!new File(this.indexPath).mkdirs()) throw new IOException("mkdir failed");
     }
 
     /**
@@ -33,7 +47,10 @@ public class MyIndexWriter {
      * you need to assign the new non-negative integer docId to each document, which will be used in MyIndexReader
      */
     public void IndexADocument(String docno, String content) throws IOException {
+        this.docIdIndex.add(docno);
+        this.totalNumOfDocument++;
 
+        String[] contents = content.split(" ");
     }
 
     /**
@@ -41,8 +58,23 @@ public class MyIndexWriter {
      * if you write your index into several files, you need to fluse them here.
      */
     public void Close() throws IOException {
-        this.indexWriter.flush();
-        this.indexWriter.close();
+        if (this.totalNumOfDocument != this.docIdIndex.size()) throw new RuntimeException("Inbalance tree!");
+
+        this.termWriterMap.values().forEach(writer -> {
+            if (writer == null) return;
+            try {
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        try (BufferedWriter idxWriter = new BufferedWriter(new FileWriter(this.indexPath + "docidx"))) {
+            int i = 0;
+            for (String data : this.docIdIndex) {
+                idxWriter.write(String.format("%s\t%d", data, i++));
+            }
+        }
     }
 
 }
